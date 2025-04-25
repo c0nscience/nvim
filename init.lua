7a39267677a887e1a47bfc5cd92108b57a0ee912
Benjamin Herzig <ber.bherzig@pm.me>
2025-04-26 01:13:47 +0200

    use powershell in windows

--- init.lua
+++ init.lua
@@ -68,6 +68,18 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv 'HOME' or os.getenv 'USERPROFILE' .. '/.vim/undodir'
 vim.opt.undofile = true
 
+local powershell_options = {
+  shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell',
+  shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+  shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+  shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+  shellquote = '',
+  shellxquote = '',
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 user_terminals = {}
